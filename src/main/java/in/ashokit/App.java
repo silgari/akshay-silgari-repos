2662f033f35344522f5317f14c0a33d010581213
Akshay <silgari.akshay@gmail.com>
2024-07-20 21:51:47 +0530

    added docker :: task-102

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -4,10 +4,8 @@ package in.ashokit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! Akshay Silgari" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Akshay Silgari. I am in solapur");
     }
 }
